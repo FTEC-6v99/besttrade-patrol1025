1e02cd6246d43f1d93b33b0b477fd4c3609a2bd9
Patrick Roland <patricklroland@gmail.com>
2021-11-01 00:01:15 -0500

    Commit

--- app/src/domain/Investor.py
+++
@@ -1,6 +0,0 @@
-class Investor():
-    # class has same attributes as the investor db table
-    def __init__(self, name, status, id = None):
-        self.id = id
-        self.name = name
-        self.status = status
