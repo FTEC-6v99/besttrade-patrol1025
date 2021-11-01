4c194d887d9643bb365e93e3dd8296c490d59639
Patrick Roland <patricklroland@gmail.com>
2021-11-01 00:00:34 -0500

    Commit

--- app/src/domain/Account.py
+++ app/src/domain/Account.py
@@ -1,3 +1,5 @@
 class Account():
-    # create a class that mimics the database table: account
-    pass
+    def __init__(self, account_number:int, investor_id:int, balance:float):
+        self.account_number=account_number
+        self.investor_id=investor_id
+        self.balance:balance
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 dbparams = {
-    'host': '',
-    'username': '',
-    'password': '',
-    'database': ''
+    'host': 'bestrade-d.c3vonzgsmuaa.us-east-2.rds.amazonaws.com', #end point aws
+    'username': 'admin',
+    'password': '1400patrick1140',
+    'database': 'BEST_TRADE'
 }
